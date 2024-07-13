190ccd2c99c3ae9fd9040091200e3f8c82f2911c
Mohd Asad Ansari <asad.as9458@gmail.com>
2024-07-13 19:47:18 +0530

    Done Routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Login from "./Pages/Login.jsx";
+import Registration from "./Pages/Registration.jsx";
+import Dashboard from "./Pages/Dashboard.jsx";
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='login' element={<Login />} />
+      <Route path='registration' element={<Registration />} />
+      <Route path='dashboard' element={<Dashboard />} />
+    </Route>
+  )
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
