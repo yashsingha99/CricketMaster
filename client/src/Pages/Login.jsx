df6aa44db8aedfb77e6ccdd40226e927b7901b2f
Mohd Asad Ansari <asad.as9458@gmail.com>
2024-07-17 19:53:08 +0530

    Login And Registration Page Styling Done

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,40 +1,43 @@
 import React from "react";
-import '../styles/Login.css'
-import { useState } from "react";
-import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
+import "../styles/Login.css";
+import { FaUser, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className={`wrapper`}>
-      <div className="form-box login">
-        <form action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input type="text" placeholder="Username" required />
-            <FaUser className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="Password" placeholder="Password" required />
-            <FaLock className="icon" />
-          </div>
+    <div className="container">
+      <div className="wrapper">
+        <div className="form-box">
+          <form action="">
+            <h1>Login</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Username" required />
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input type="Password" placeholder="Password" required />
+              <FaLock className="icon" />
+            </div>
 
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" />
-              Remember me
-            </label>
-            <a href="#">Forgot password?</a>
-          </div>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" />
+                Remember me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
 
-          <button type="submit">Login</button>
+            <div className="main-btn">
+              <button type="submit">Login</button>
+            </div>
 
-          <div className="register-link">
-            <p>
-              Don't have an account? <Link to="/registration">Register</Link>
-            </p>
-          </div>
-        </form>
+            <div className="register-link">
+              <h4>
+                Don't have an account? <Link to="/registration">Register</Link>
+              </h4>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/Pages/Registration.jsx
+++ client/src/Pages/Registration.jsx
@@ -5,37 +5,41 @@ import "../styles/Login.css";
 
 const Registration = () => {
   return (
-      <div className="form-box register">
-        <form action="">
-          <h1>Register</h1>
-          <div className="input-box">
-            <input type="text" placeholder="Username" required />
-            <FaUser className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="email" placeholder="Email" required />
-            <FaEnvelope className="icon" />
-          </div>
-          <div className="input-box">
-            <input type="Password" placeholder="Password" required />
-            <FaLock className="icon" />
-          </div>
+    <div className="container">
+      <div className="wrapper">
+        <div className="form-box">
+          <form action="">
+            <h1>Register</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Username" required />
+              <FaUser className="icon" />
+            </div>
+            <div className="input-box">
+              <input type="email" placeholder="Email" required />
+              <FaEnvelope className="icon" />
+            </div>
+            <div className="input-box">
+              <input type="Password" placeholder="Password" required />
+              <FaLock className="icon" />
+            </div>
 
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" />I agree to the terms & conditions
-            </label>
-          </div>
-
-          <button type="submit">Register</button>
-
-          <div className="register-link">
-            <p>
-              Already have an account? <Link to="/Login">Login</Link>
-            </p>
-          </div>
-        </form>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" />I agree to the terms & conditions
+              </label>
+            </div>
+            <div className="main-btn">
+              <button type="submit">Register</button>
+            </div>
+            <div className="register-link">
+              <h4>
+                Already have an account? <Link to="/Login">Login</Link>
+              </h4>
+            </div>
+          </form>
+        </div>
       </div>
+    </div>
   );
 };
 export default Registration;
