f50aaf734f580287dc9eb864ad1a123a5561cda5
Yash Singhal <singhaly914@gmail.com>
2024-07-18 17:08:32 +0530

    add

---
+++ backend/src/models/matches.mode.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const matchSchema = mongoose.Schema(
+   {
+     matchName : {
+        type : String,
+        require : true 
+     },
+     
+     teams : [
+        {
+          type : mongoose.Schema.Types.ObjectId,
+          ref  : Team
+        },
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref  : Team
+        }
+     ],
+    
+    // abounded, T1 win, T2 win, match cancled
+     matchStatus : {
+        Type : String,
+        require : true
+     },
+    
+     
+
+
+   },
+   {
+       timestamps:true 
+   }
+)
