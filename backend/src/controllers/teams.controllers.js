05659eb63258a6440612f0e1c1731da8d8f6caa4
Yash Singhal <singhaly914@gmail.com>
2024-08-07 11:02:48 +0530

    add new controllers for player

--- backend/src/controllers/teams.controllers.js
+++ backend/src/controllers/teams.controllers.js
@@ -1,5 +1,5 @@
 const Player = require("../models/players.model")
-
+const Team = require("../models/teams.model")
 const addTeam = async(req, res) => {
 try {
       const {name} = req.body;
@@ -17,6 +17,21 @@ try {
 }
 
 const addPlayer = async(req, res) => {
+  const {Player, team} = req.body;
+  if(!Player || !team) 
+      return res.status(400).json({message: "Insufficient data"})
+  const findPlayer = await Player.findById(Player._id);
+  if(!findPlayer) 
+    return res.status(400).json({message: "player not found"})
+  const findTeam = await Player.findById(team._id);
+  if(!findTeam) 
+    return res.status(400).json({message: "team not found"})
+  const addPlayer = await Team.findByIdAndUpdate(
+    findTeam._id,
+    {
+      $addToSet: {Player : Player}
+    }
+  )
   
 }
 
--- backend/src/models/teams.model.js
+++ backend/src/models/teams.model.js
@@ -26,6 +26,7 @@ const teamSchema = new mongoose.Schema({
       ref: "Match",
     },
   ],
+  
   avatar: {
     type: String,
     default:
